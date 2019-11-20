8a08f9d8dba3ba981f6b07a97fa49d719617837a
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-11-20 19:01:54 +0100

    done with app my resto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Ramen-Ya',
+    address:      'Place Commerces, Nantes',
+    phone_number: '02 40 45 78 96',
+    category:     'japanese',
+  },
+  {
+    name:         'Le Petit François',
+    address:      'Ile de Nantes, Nantes',
+    phone_number: '02 40 45 23 14',
+    category:     'french',
+  },
+  {
+    name:         'Le Louis Blanc',
+    address:      'Ile de Nantes, Nantes',
+    phone_number: '02 45 21 52 66',
+    category:     'french',
+  },
+  {
+    name:         'Spritz baby',
+    address:      'Ile de Nantes, Nantes',
+    phone_number: '02 78 52 52 36',
+    category:     'italian',
+  },
+  {
+    name:         'Le Waffle',
+    address:      'Ile de Nantes, Nantes',
+    phone_number: '02 78 42 56 66',
+    category:     'belgian',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
